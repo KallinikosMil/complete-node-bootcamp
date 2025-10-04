4d925ebac815d8de4d43d6bd4c5e96e6a0953f58
Kallinikos <kallinikosmil@gmail.com>
2025-10-04 14:46:03 +0300

    git ignore and asynch js reminder

---
+++ 3-asynchronous-JS/starter/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const superagent = require('superagent');
+
+const readFilePro = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, (err, data) => {
+      if (err) reject('I could not find the file');
+      resolve(data);
+    });
+  });
+};
+
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (error) => {
+      if (error) reject('HEHE HOHO');
+      resolve('success');
+    });
+  });
+};
+
+readFilePro(`${__dirname}/dog.txt`)
+  .then((data) => {
+    console.log(`Breed: ${data}`);
+    return superagent.get(`https://dog.ceo/api/breed/${data}/images/random`);
+  })
+  .then((res) => {
+    console.log(res.body.message);
+
+    return writeFilePro('.dog-img.txt', res.body.message);
+  })
+  .then(() => console.log('Random dog image saved to file!'))
+  .catch((err) => console.log(err));
